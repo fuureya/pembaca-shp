8ff7577264988a061fbb80f079f385da25b1c0e8
Adi Kautsar <agiljibrin009@gmail.com>
2024-09-17 16:05:42 +0800

    test: membaca file shp dan menjadikan nya dalam bentuk geojson

--- app/Http/Controllers/ShpReader.php
+++ app/Http/Controllers/ShpReader.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Shapefile\Shapefile;
+use Shapefile\ShapefileException;
+use Shapefile\ShapefileReader;
 
 class ShpReader extends Controller
 {
-    //
+    public function Pembaca()
+    {
+        $Shapefile = new ShapefileReader(public_path("shp/Wilayah_Adm_Desa_Bukit_Makmur.shp"));
+       
+        while ($Geometry = $Shapefile->fetchRecord()) {
+            if ($Geometry->isDeleted()) {
+                continue;
+            }
+           $data = $Geometry->getGeoJSON();
+        }
+
+        return view("index", ["data" => $data]);
+    }
 }
